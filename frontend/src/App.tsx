557c0278059b0d5a3304920164870cd04cc85eed
Fernando <colomfernando@gmail.com>
2022-07-30 11:21:07 -0300

    setup docker for frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyles from './theme/GlobalStyles';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <div>Hi</div>
+    </>
+  );
+};
+
+export default App;
