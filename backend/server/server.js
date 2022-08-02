3489e55ca7674cffaa4c8dd406a8cbfa0405687d
Fernando <colomfernando@gmail.com>
2022-08-02 20:21:56 -0300

    add router

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const postRouter = require('./routes/posts');
+const userRouter = require('./routes/users');
 
 const server = express();
 const PORT = 8080;
 
-server.get('/', (req, res) => {
+server.use(bodyParser.json());
+server.use(
+	bodyParser.urlencoded({
+		extended: true,
+	})
+);
+
+server.use('/api/posts', postRouter);
+server.use('/api/users', userRouter);
+
+server.get('/', async (req, res) => {
 	res.send('Hello server');
 });
 
