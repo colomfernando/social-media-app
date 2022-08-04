0de5b8c9f3367bf16929636945c32b7ad9567087
Fernando <colomfernando@gmail.com>
2022-08-03 21:48:16 -0300

    add get post

--- backend/server/routes/post/index.js
+++ backend/server/routes/post/index.js
@@ -6,8 +6,7 @@ const router = express.Router();
 router.get('/', async (req, res, next) => {
 	try {
 		const post = await Post.find({});
-		console.log('post :>> ', post);
-		res.status(200).send('Hello from router post');
+		res.status(200).send(post);
 	} catch (error) {
 		next(error);
 	}
