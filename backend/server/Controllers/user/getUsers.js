6b8039da79753eba01bb4f6f26f77d543859636f
Fernando <colomfernando@gmail.com>
2022-08-27 12:02:48 -0300

    add search user

--- backend/server/Controllers/user/getUsers.js
+++ backend/server/Controllers/user/getUsers.js
@@ -1,10 +1,19 @@
 const asyncWrapper = require('../../utils/asyncWrapper');
 const User = require('../../models/user');
 const ErrorHandler = require('../../modules/ErrorHandler');
+const Jwt = require('../../modules/Jwt');
 
 const getUsers = async (req, res, next) => {
   try {
-    const [users, error] = await asyncWrapper(() => User.find({}));
+    const { user } = req.query;
+    const token = req.header('auth-token') || req.cookies['auth-token'];
+    const userIdToken = Jwt.userId(token);
+
+    const searchByUser = user ? { username: { $regex: user } } : {};
+
+    const [users, error] = await asyncWrapper(() =>
+      User.find({ _id: { $ne: userIdToken }, ...searchByUser })
+    );
 
     if (error) throw new ErrorHandler();
     res.status(200).send(users);
